e9454307a8904d258db9b40312dd68feea4094b5
luis11! <131817446+luispneu@users.noreply.github.com>
2024-06-19 16:07:59 -0400

    Add files via upload

---
+++ shop-main (1)/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "fakestoreapi.com", 
+            "www.flaticon.com", 
+            "cdn-icons-png.flaticon.com", 
+            "br.pinterest.com", 
+            "icon-icons.com", 
+            "pt.vecteezy.com",
+            "static.vecteezy.com",
+            "th.bing.com"
+        ]
+    }
+};
+
+export default nextConfig;
